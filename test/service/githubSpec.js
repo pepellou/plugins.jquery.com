115ae2c6db27f3b3f29fbe0032b2b8b2cd8cafe4
Pepe Doval <pepellou@gmail.com>
2012-04-08 21:07:34 +0200

    Refactor

--- test/service/githubSpec.js
+++ test/service/githubSpec.js
@@ -4,9 +4,9 @@ describe("The github service", function () {
 	});
 
 	it("has a dirname() function that computes the dir name of a path", function () {
-		var aPath = "a/path/with/a/file.txt";
-		var expectedDirname = "a/path/with/a";
-		expect( dirname(aPath) ).toEqual(expectedDirname);
+		var dirName = "a/path/with/a";
+		var fileName = "file.txt";
+		expect( dirname(dirName + '/' + fileName) ).toEqual(dirName);
 	});
 
 });
